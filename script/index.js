8c8f1a85212e083f9baa7cc7992dff3bbad46d5c
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-09-21 23:14:46 +0600

    Initial commit

---
+++ script/index.js
@@ -0,0 +1,21 @@
+document.getElementById('sign-in-btn').addEventListener('click', function(){
+    const userNameInput = document.getElementById('user-name');
+
+    const userName = userNameInput.value;
+
+    const passwordInput = document.getElementById('password');
+
+    const password = passwordInput.value
+
+    if (userName === 'admin' && password === 'admin123') {
+        
+        alert('You have signed in successfully')
+
+        window.location.assign('./home.html')
+    }
+
+    else{
+       alert('Wrong Credentials. Please try again!') 
+    }
+
+})
